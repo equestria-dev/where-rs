e71a89aaddf476f7fa9709cea22b8297ac180c56
RaindropsSys <raindrops@equestria.dev>
2024-06-24 23:54:38 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-whered_version=$(cargo pkgid whered | tr "#" " " | awk '{print $2;}')
-where_version=$(cargo pkgid where-rs | tr "#" " " | awk '{print $2;}')
-rustc_version=$(rustc --version | awk '{print $2;}')
-
 cargo clean
 cargo build --target aarch64-apple-darwin --release
 cargo build --target x86_64-apple-darwin --release
